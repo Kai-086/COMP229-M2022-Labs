aed230f0a9641affd1fa83a1f56f407eddc0f4d1
Kai <bono6415@gmail.com>
2022-06-18 00:49:34 -0400

    added utility functions

---
+++ Week6/Server/Util/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+// Convenience function to return the DisplayName of the User
+export function UserDisplayName(req: Express.Request): string {
+    if(req.user) {
+        let user = req.user as UserDocument
+        return user.DisplayName.toString();
+    }
+    return ''; // If user not exist
+}
+
+// Helper function for guarding secure locations
+export function AuthGuard(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if(!req.isAuthenticated) {
+        return res.redirect('/login'); // If not authenticated, go back to login page
+    }
+    next();
+}
